697b153f8225c9da9d7cfa0d704ac052ba77eec6
unio <unioblog@gmail.com>
2018-10-09 01:53:46 +0900

    問い合わせ機能完成

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
   
   default from: "info@twesumecard.herokuapp.com"
-  default to: "unioblog@gmail.com"
+  default bcc: "unioblog@gmail.com"
 
   def received_email(contact)
     @contact = contact
     mail_subject = "TwesumeCard " + @contact.name + "様からのお問い合わせ"
-    mail(subject: mail_subject)
+    mail(to: @contact.email, subject: mail_subject)
   end
 end
