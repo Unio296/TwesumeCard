7047060dca19f198b0a3d3fc86794c71002ffb3d
unio <unioblog@gmail.com>
2018-10-09 01:11:25 +0900

    修正２

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ActionMailer::Base
   
   default from: "info@独自ドメイン"
-  # default to: "送信先"
+  default to: "unioblog@gmail.com"
 
   def received_email(contact)
     @contact = contact
-    mail(to: @contact.email, subject: 'お問い合わせありがとうございます。')
+    mail_subject = "TwesumeCard " + @contact.name + "様からのお問い合わせ"
+    mail(subject: mail_subject)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # メール送信設定追加
+  config.action_mailer.default_url_options = {host: 'localhost', port: 80 }
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
