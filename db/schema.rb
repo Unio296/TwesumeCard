8125d6f0e460c3962febad6b88efb604f4dd277f
unio <unioblog@gmail.com>
2018-10-21 10:47:16 +0900

    migrate順序変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_222742) do
+ActiveRecord::Schema.define(version: 2018_10_08_222158) do
 
   create_table "resumes", force: :cascade do |t|
     t.string "job_type"
