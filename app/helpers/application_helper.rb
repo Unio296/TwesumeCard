f58e61f546c6c00a8cbbe5a043879ad20716591c
unio <unioblog@gmail.com>
2018-11-01 00:09:44 +0900

    タイトル修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "TWESUME CARD"
+    base_title = "TWESUME"
     if page_title.empty?                                        #page_titleが空白の場合
       base_title
     else                                                        #page_titleが空白でない場合
@@ -9,10 +9,9 @@ module ApplicationHelper
     end
   end
 
-  # 改行を<br>に変換した後、htmlのコードにする
-  def newline2br_html(str)
-    return nil if str.empty?
-    str.gsub(/(\r\n|\r|\n)/, "<br>").html_safe
+  # 改行を<br>に変換する
+  def newline2br(str)
+    str.gsub(/(\r\n|\r|\n)/, "<br>")
   end
 
 
