7bfc4b80290202bec4f7fc155f37f39c7b26fc47
unio <unioblog@gmail.com>
2018-11-12 15:25:45 +0900

    domain変更herokuからのリダイレクト

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,7 +2,7 @@
 
 if ENV['RACK_ENV'] == 'production'
   TwesumeCard::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'https://twesume.work$&', :if => Proc.new {|rack_env|
+    r301 %r{.*}, 'https://twesumes.net$&', :if => Proc.new {|rack_env|
       rack_env['SERVER_NAME'] == 'twesume.herokuapp.com'
     }
   end
